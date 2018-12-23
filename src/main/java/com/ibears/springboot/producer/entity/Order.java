1d9e0d4b00609d0c94a0d5d8f7bc2f04a87f0f9d
xiaoxiong <734676682@qq.com>
2018-12-23 14:20:55 +0800

    测试更新

--- src/main/java/com/ibears/springboot/producer/entity/Order.java
+++ src/main/java/com/ibears/springboot/producer/entity/Order.java
@@ -8,9 +8,7 @@ import java.io.Serializable;
  */
 public class Order implements Serializable {
     
-    
     private static final long serialVersionUID = 5862941467077065519L;
-    
     
     private String orderId;
     
